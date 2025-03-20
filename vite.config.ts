5eaa8f915bb3779be18d4e6e525034a08fac7954
nguyendanghung97 <nguyendanghung97@hotmail.com>
2025-03-20 14:23:32 +0700

    change vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: './build',
+    outDir: 'dist',
   },
   resolve: {
     alias: [
